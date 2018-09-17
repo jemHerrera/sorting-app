7f0a02f51fbe0ea1f1fe6a1650d98d60f306d785
Jem Herrera <jhneramuz@gmail.com>
2018-09-17 14:42:55 -0600

    finished color sorting, need input

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ new Vue({
     data:{
         pageMaxCount : 6,
         pageAmount : 0,
+
+        // STATIC CONTENT
         items:[
             {
                 name: "Astronomy Infographic",
-                source: "images/astronomy.jpg",
+                source: {
+                    Dark: "images/astronomy.jpg"
+                },
+                activeTheme: "Dark",
                 topic: ["Science"],
                 type: ["Informational"],
                 theme: ["Dark"],
@@ -14,7 +19,11 @@ new Vue({
             },
             {
                 name: "Healthy Food and Balanced Diet",
-                source: "images/balanced-diet.jpg",
+                source: {
+                    Light: "images/balanced-diet.jpg",
+                    Warm: "images/balanced-diet-warm.jpg"
+                },
+                activeTheme: "Light",
                 topic: ["Food", "Health"],
                 type: ["Statistical"],
                 theme: ["Warm", "Light"],
@@ -22,7 +31,10 @@ new Vue({
             },
             {
                 name: "Blue Light",
-                source: "images/blue-light.png",
+                source: {
+                    Cool: "images/blue-light.png",
+                },
+                activeTheme: "Cool",
                 topic: ["Health"],
                 type: ["Informational", "Statistical"],
                 theme: ["Cool"],
@@ -31,6 +43,11 @@ new Vue({
             {
                 name: "Smartphone vs Sleep",
                 source: "images/blue-light-vs-sleep.png",
+                source: {
+                    Light: "images/blue-light-vs-sleep.png",
+                    Cool: "images/blue-light-vs-sleep-cool.png"
+                },
+                activeTheme: "Light",
                 topic: ["Health"],
                 type: ["Informational", "Timeline"],
                 theme: ["Cool", "Light"],
@@ -39,6 +56,12 @@ new Vue({
             {
                 name: "Brain Infographics Elements",
                 source: "images/brain-infographics.jpg",
+                source: {
+                    Light: "images/brain-infographics.jpg",
+                    Cool: "images/brain-infographics-cool.jpg",
+                    Warm: "images/brain-infographics-warm.jpg"
+                },
+                activeTheme: "Light",
                 topic: ["Health", "Science"],
                 type: ["Informational"],
                 theme: ["Cool", "Light", "Warm"],
@@ -173,46 +196,64 @@ new Vue({
                 link: "pages/healthy-food-infographic.html"
             },
         ],
+        
+        // FILTERED ITEMS
+        filteredItems: [],
+        
+        // SORTED ITEMS INTO PAGES
         displayedItems:[],
+        
         sorting : {
             topic: false,
             type: false,
             colorTheme: false,
         },
+        currentPage : 1,
         searchActive: false,
         appliedFilter: [],
         search: "",
     },
     mounted(){
-        this.pageAmount = Math.floor((this.items.length-1)/this.pageMaxCount)+1;
-        for(let i=1; i<=this.pageAmount; i++){
-            this.displayedItems.push([]);
-            if(i == this.pageAmount){
-                for(let j=0; j<=this.pageMaxCount-1; j++){
-                    this.displayedItems[i-1].push(this.items[(i-1)*this.pageMaxCount+j]);
-                    
-                    console.log(this.items[(i-1)*this.pageMaxCount+j].name);
-                }
-            }
-            else for(let j=0; j<=this.pageMaxCount-1; j++){
-                this.displayedItems[i-1].push(this.items[(i-1)*this.pageMaxCount+j]);
-                console.log(this.items[(i-1)*this.pageMaxCount+j].name);
-            }
-        }
+        // SETUP THE PAGES
+        this.filteredItems = this.items;
+        this.dividePages();
     },
     watch:{
+        // IF FILTER CHANGES, CHANGE THE DOM ACCORDINGLY
         appliedFilter: function(){
-            this.displayedItems = this.items.filter(item => {
+            this.filteredItems = this.items.filter(item => {
                 let tags = item.topic.concat(item.type, item.theme);
                 let counter = 0;
                 for(let filter of this.appliedFilter){
                     if(tags.includes(filter)) counter++;
+                    if(item.theme.includes(filter)){
+                        item.activeTheme = filter;
+                    }
                 }
                 return (counter == this.appliedFilter.length && item.name.toLowerCase().match(this.search.toLowerCase()));
             });
+            this.dividePages();
+            this.currentPage = 1;
+        }
+    },
+    computed:{
+        // OPTIONAL PREVIOUS PAGE BUTTON
+        displayPrevious: function(){
+            return(this.currentPage != 1);
+        },
+        // OPTIONAL NEXT PAGE BUTTON
+        displayNext: function(){
+            return(this.currentPage < this.pageAmount);
+        },
+        // CHANGE IMAGE ACCORDING TO THE SORTED THEME
+        imgSource: function(){
+            for(let item of this.filteredItems){
+                let tags = item.topic.concat(item.type, item.theme);
+            }
         }
     },
     methods:{
+        // SWITCH SORTING CATEGORY
         changeSorting: function(type){
             let vm = this;
             let active = ""
@@ -226,18 +267,55 @@ new Vue({
                 }, 300)
             } 
         },
+        // REMOVE FILTER FROM FILTER ARRAY
         removeFilter: function(filter){
             this.appliedFilter.splice(this.appliedFilter.indexOf(filter), 1);
         },
+        // ADD FILTER TO FILTER ARRAY
         applyFilter: function(event){
             if(this.appliedFilter.indexOf(event.target.textContent) == -1)this.appliedFilter.push(event.target.textContent);
         },
+        // FILTER ITEMS BASED ON SEARCH
         applySearch: function(){
             this.appliedFilter = [];
             console.log(this.search);
-            this.displayedItems = this.items.filter((item) => {
+            this.filteredItems = this.items.filter((item) => {
                 return item.name.toLowerCase().match(this.search.toLowerCase());
             });
+        },
+        // DIVIDE THE ITEMS INTO A NUMBER OF PAGES
+        dividePages: function(){
+            this.displayedItems = [];
+            this.pageAmount = Math.floor((this.filteredItems.length-1)/this.pageMaxCount)+1;
+            let remainder = Math.floor((this.filteredItems.length-1)%this.pageMaxCount)+1;
+            for(let i=1; i<=this.pageAmount; i++){
+                this.displayedItems.push([]);
+                if(i == this.pageAmount){
+                    for(let j=0; j<=remainder-1; j++){
+                        this.displayedItems[i-1].push(this.filteredItems[(i-1)*this.pageMaxCount+j]);
+                    }
+                }
+                else {
+                    for(let j=0; j<=this.pageMaxCount-1; j++){
+                        this.displayedItems[i-1].push(this.filteredItems[(i-1)*this.pageMaxCount+j]);
+                    }
+                }
+            }
+        },
+        // CHANGE COLOR OF BUTTONS
+        buttonColor: function(color){
+            if(color == 'Light') return {
+                'background-color': 'white'
+            }
+            else if(color == 'Dark') return {
+                'background-color': '#222222'
+            }
+            else if(color == 'Warm') return {
+                'background-color': '#c74511'
+            }
+            else if(color == 'Cool') return {
+                'background-color': '#1148c7'
+            }
         }
     }
 });
